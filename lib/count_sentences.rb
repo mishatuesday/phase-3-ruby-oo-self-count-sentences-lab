d429a8218ace60c66cc1ccf7a6049fcc46bb605c
Misha Tuesday <mishatuesday@gmail.com>
2022-11-01 09:32:17 -0400

    commit tree acfe6bdb212d0a4a07a4e061d74c700b3fea40ae

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/.*\.$/) ? true : false
   end
 
   def question?
-
+    self.match(/.*\?$/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/.*!$/) ? true : false
   end
 
   def count_sentences
 
   end
 end
+
